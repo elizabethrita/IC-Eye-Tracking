b13a8f6be2f84c93dc9c431c3e4cf33b98ac9096
elizabethrita <rita.elizabeth@geb.inatel.br>
2022-03-23 21:39:15 -0300

    marcadores

--- exibicao.py
+++ exibicao.py
@@ -11,12 +11,12 @@ from scipy.stats import iqr
 
 def freedman_diaconis_rule(coords):
 
-    data_x = coords[:,1]
+    data_x = coords[:, 1]
     bin_width_x = 2.0 * iqr(data_x) / np.power(len(data_x), 1.0/3.0)
     bin_count_x = int(np.ceil((np.max(data_x) - np.min(data_x)) / bin_width_x))
     print(f'bin_width_x = {bin_width_x}\tbin_count_x = {bin_count_x}')
 
-    data_y = coords[:,2]
+    data_y = coords[:, 2]
     bin_width_y = 2.0 * iqr(data_y) / np.power(len(data_y), 1.0/3.0)
     bin_count_y = int(np.ceil((np.max(data_y) - np.min(data_y)) / bin_width_y))
     print(f'bin_width_y = {bin_width_y}\tbin_count_y = {bin_count_y}')
@@ -25,11 +25,24 @@ def freedman_diaconis_rule(coords):
 
 def exibir(coords):
 
+
     print('Showing heatmap.')
     bin_count_x,bin_count_y = freedman_diaconis_rule(coords)
     colors = np.random.rand(len(coords))
     fig = px.density_heatmap(coords, x=coords[:,1], y=coords[:,2], nbinsx=bin_count_x, nbinsy=bin_count_y)
+   
+    fig.add_traces(
+    go.Scatter(
+        x=[610,875],y=[610,625], mode="markers", marker=dict(
+            color='LightSkyBlue',
+            size=20), name="Ground Truth", hoverinfo="skip" 
+    )
+    )
     fig.show()
+   
+
+   
+
 
     #print('Showing pizza plot.')
     #labels = ['Left_Gaze', 'Right_Gaze']
